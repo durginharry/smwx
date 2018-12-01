e8d574c8eb10bf7dadc1b7b0f78dd7ad33405fdf
harry <durgin.harry@gmail.com>
2018-11-30 15:29:10 -1000

    add lat long to file

--- www/js/index.js
+++ www/js/index.js
@@ -16,7 +16,7 @@ var photograph=function() {
       CameraPreview.takePicture({height:1280, width:720, quality:65}, function(base64PictureData) {
         var pic='data:image/jpeg;base64,'+base64PictureData;
         
-        $.post(url, {image: pic, lat: lat, lon: 345, timeout: 50000}, function(data, status, xhr) {
+        $.post(url, {image: pic, lat: lat, lon: lon, timeout: 50000}, function(data, status, xhr) {
                             alert('return: ' + status + '\nData: ' + data);
                         }
                     )
