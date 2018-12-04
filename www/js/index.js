d82d7aee8edda9f13d748668dbeb42356cee5113
harry <durgin.harry@gmail.com>
2018-12-03 18:31:34 -1000

    removed alert from post.fail

--- www/js/index.js
+++ www/js/index.js
@@ -15,14 +15,7 @@ var photograph=function() {
       CameraPreview.setFlashMode('off');
       CameraPreview.takePicture({height:1280, width:720, quality:65}, function(base64PictureData) {
         var pic='data:image/jpeg;base64,'+base64PictureData;
-        
-        $.post(url, {image: pic, lat: lat, lon: lon, timeout: 50000}, function(data, status, xhr) {
-//                            alert('return: ' + status + '\nData: ' + data);
-                        }
-                    )
-                    .fail(function(error, status, xhr) {
-                        alert('error: ' + status + '\nReason: ' + xhr);
-                    });
+        $.post(url, {image: pic, lat: lat, lon: lon, timeout: 50000}, function(data, status, xhr) { }).fail(function(error, status, xhr) { });
       });
 }
 
