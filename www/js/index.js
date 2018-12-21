d46fce0ee20fa2696cdea52ce813952e045c3446
harry <durgin.harry@gmail.com>
2018-12-20 14:37:22 -1000

    additional params

--- www/js/index.js
+++ www/js/index.js
@@ -26,8 +26,8 @@ var photograph=function() {
 var params=function() {
   let url='http://smwx.org/params.php';
   $.post(url, {uuid: uuid, timeout: 5000}, function(data, status, xhr) {
-alert(data);
     $params = explode(',',data);
+alert($params);
     interval = $params[0]*1000;
     image_width = $params[1];
     image_quality = $params[2]; 
